f5d8fc7da88482ac02bf65a7f0ed85527610aa50
Anand <anand.thakur@toddleapp.com>
2024-01-08 17:35:53 +0530

    dotenv

--- server/index.js
+++ server/index.js
@@ -4,11 +4,12 @@ const schema = require('./schema/index.js');
 const resolver = require('./resolver/index.js');
 const isAuth = require('./middleware/is-auth');
 // const connectDB = require('./config/db');
+require("dotenv").config()
 const port = process.env.PORT || 5000;
 const mongoose = require("mongoose")
 const app = express();
  
-mongoose.connect(`mongodb+srv://kali:kali@cluster0.msujdem.mongodb.net/booking?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.msujdem.mongodb.net/booking?retryWrites=true&w=majority`)
 .then(()=>{
     app.listen(port, console.log(`Server running on port ${port}`));
 })
